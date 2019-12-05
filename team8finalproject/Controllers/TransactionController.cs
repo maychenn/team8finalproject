ac5a90aad8b128fd452b462f942f9581ce454546
shayan-ali <shayanali1@icloud.com>
2019-12-05 03:21:27 -0600

    Transaction Stuff
    Still doesn't show view

--- team8finalproject/Controllers/TransactionController.cs
+++ team8finalproject/Controllers/TransactionController.cs
@@ -13,8 +13,9 @@ namespace team8finalproject.Controllers
     public class TransactionController : Controller
     {
         private readonly AppDbContext _context;
+		private readonly UserManager<AppUser> _userManager;
 
-        public TransactionController(AppDbContext context)
+		public TransactionController(AppDbContext context)
         {
             _context = context;
         }
@@ -46,7 +47,10 @@ namespace team8finalproject.Controllers
         // GET: Transaction/Create
         public IActionResult Create()
         {
-            return View();
+			Viewbag.SelectType = Enum.GetValues(typeof(TransactionTypes)).Cast<TransactionTypes>();
+			ViewBag.AllProducts = GetAllProducts();
+			ViewBag.Accounts = FindAccounts();
+			return View();
         }
 
         // POST: Transaction/Create
@@ -54,20 +58,65 @@ namespace team8finalproject.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TransactionID,Number,Description,Date,Amount,TransactionType,TransactionStatus")] Transaction transaction)
+        public async Task<IActionResult> Create([Bind("TransactionID,Number,Description,Date,Amount,TransactionType,TransactionStatus")] Transaction transaction, int SelectedProduct, int SelectedTransaction)
         {
 
-            Product product = _context.Products.Find(transaction);
-            transaction.Product = product;
+			//Find the Selected Transaction
+			Transaction transaction = _context.Transactions.Find(SelectedTransaction);
+			transaction.Transactions = transaction;
 
-            transaction.TransactionType = TransactionTypes.Deposit;
+			////Find the selected Account
+			Product product = _context.Products.Find(SelectedProduct);
+			transaction.Product = product;
 
 
-            if (ModelState.IsValid)
+			transaction.TransactionType = TransactionTypes.Deposit;
+
+			transaction.Product.Balance = transaction.Product.Balance + transaction.Amount;
+
+			if (transaction.Amount <= 0)
+			{
+				ViewBag.Error = "Amount must be greater than $0.00.";
+				return View(transaction);
+			}
+
+
+			if (transaction.Amount >= 5000)
+			{
+
+				transaction.TransactionStatus = TransactionStatus.Pending;
+				ViewBag.LargeDepositMessage = "Your deposit is $5000 or larger. You need to wait on Manager Approval";
+
+			}
+
+			else
+			{
+
+				product.AccountBalance = product.AccountBalance + transaction.Amount;
+
+			}
+
+			Transaction deposit = new Transaction();
+			deposite.Description = "New Desposit";
+			deposit.Date = transaction.Date;
+			deposit.Number = transaction.Number;
+			deposit.TransactionType = TransactionTypes.Deposit;
+			deposit.Product = transaction.Product;
+			_context.Transactions.Add(deposit);
+			transaction.Product.AccountBalance += deposit.Amount;
+			_context.Transactions.Add(transaction);
+			_context.SaveChangesAsync();
+			ViewBag.Message = "Successful Deposit";
+
+			return RedirectToAction("Index", "Transaction", new { id = transaction.TransactionID});
+		
+
+
+			if (ModelState.IsValid)
             {
                 _context.Add(transaction);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Create", "Transaction", new { id = transaction.Product.ProductID });
+                return RedirectToAction("Index", "Transaction", new { id = transaction.TransactionID });
 
             }
             return View(transaction);
@@ -94,12 +143,15 @@ namespace team8finalproject.Controllers
         //POST: Deposit
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Deposit([Bind("TransactionID,Date,TransactionType,Description,Amount,TransactionStatus")] Transaction transaction, Int32 ProductID)
+        public ActionResult Deposit([Bind("TransactionID,Date,TransactionType,Description,Amount,TransactionStatus")] Transaction transaction)
         {
             if (ModelState.IsValid)
             {
 
-                Product product = _context.Products.Find(ProductID);
+                Product product = _context.Products.Find(SelectedAccount);
+
+
+
                 ViewBag.AllProducts = GetAllProducts();
                 if (transaction.Amount <= 0)
                 {
@@ -207,16 +259,40 @@ namespace team8finalproject.Controllers
 
         }
 
+		public SelectList FindAccounts()
+		{
+			var query = from p in _context.Products
+						select p;
+			{
+				query = query.Where(p => p.ProductID == ProductID);
+			}
+
+			List<Product> accountList = query.ToList();
+			IEnumerable<SelectListItem> selectList = from p in query
+													 select new SelectListItem
+													 {
+														 Value = p.ProductID.ToString(),
+														 Text = p.AccountName + " -- " + p.AccountBalance.ToString()
+													 };
+			SelectList accountSelection = new SelectList(selectList, "Value", "Text");
+			return accountSelection;
+			/*SelectList accountSelection = new SelectList(accountList, "ProductID", "Text");
+            return accountSelection;*/
+		}
 
 
 
 
-    }
+
+	}
 
 }
 
 
-  
+
+
+
+
 
 
         //// GET: Transaction/Edit/5
